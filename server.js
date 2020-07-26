a9a943c39126372306085b5344a89b7815f2efcc
Steffi Joel <steffijerome0809>
2020-07-26 14:45:24 -0400

    changes mongoose.connect

--- server.js
+++ server.js
@@ -8,24 +8,27 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 //use logger
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 //parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //use static files
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack", {useNewUrlParser: true});
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout';
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
 //require('./seeders/seed')
 
 //use routes
-require('./routes/api-routes')(app)
-require('./routes/html-routes')(app)
-
+require('./routes/api-routes')(app);
+require('./routes/html-routes')(app);
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}..`);
-})
+  console.log(`App running on port ${PORT}..`);
+});
